6e4d3328faa913d7ec60d6fa2cb69531d19724f9
notNullThen <epekaryuk@outlook.com>
2023-05-16 09:49:21 +0300

    num-mode version

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,20 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace Time_Calculator
+namespace Time_Calc_TXT
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -14,124 +27,291 @@ namespace Time_Calculator
             // Add a handler for PreviewKeyDown event
             this.AddHandler(Keyboard.PreviewKeyDownEvent, new KeyEventHandler(OnPreviewKeyDown));
         }
-        public void calculateBtn_Click(object sender, RoutedEventArgs e)
-        {
-            TimeCalculator();
-        }
         private void OnPreviewKeyDown(object sender, KeyEventArgs e)
         {
+            string time1stTxt = time1stTxtBox.Text;
+            string time1stNums = "";
+
+            string time2ndTxt = time2ndTxtBox.Text;
+            string time2ndNums = "";
+
+            for (int i = 0; i < time1stTxt.Length; i++)
+            {
+                if (time1stTxt[i] != ':')
+                {
+                    time1stNums += time1stTxt[i];
+                }
+            }
+            for (int i = 0; i < time2ndTxt.Length; i++)
+            {
+                if (time2ndTxt[i] != ':')
+                {
+                    time2ndNums += time2ndTxt[i];
+                }
+            }
+
             if (e.Key == Key.Enter)
             {
                 calculateBtn_Click(sender, e);
                 e.Handled = true; // Optional: prevent other controls from handling the event
-                dayTxtBox2nd.Focus();
             }
             if (e.Key == Key.Delete)
             {
                 resetBtn_Click(sender, e);
                 e.Handled = true; // Optional: prevent other controls from handling the event
             }
+
+            // Backspace button
+            if (e.Key == Key.Back && time2ndTxtBox.Text.Length > 0)
+            {
+                time2ndTxtBox.Text = time2ndTxtBox.Text.Remove(time2ndTxtBox.Text.Length - 1);
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Back && time1stTxtBox.Text.Length > 0 && time1stNums.Length < 9)
+            {
+                time1stTxtBox.Text = time1stTxtBox.Text.Remove(time1stTxtBox.Text.Length - 1);
+                e.Handled = true;
+            }
+
+            if (time1stNums.Length < 8)
+            {
+                if (time1stNums.Length % 2 == 0 && e.Key != Key.Back && time1stNums.Length > 0)
+                {
+                    time1stTxtBox.Text += ":";
+                }
+
+                if (e.Key == Key.Enter)
+                {
+                    calculateBtn_Click(sender, e);
+                    e.Handled = true; // Optional: prevent other controls from handling the event
+                }
+                if (e.Key == Key.Delete)
+                {
+                    resetBtn_Click(sender, e);
+                    e.Handled = true; // Optional: prevent other controls from handling the event
+                }
+
+                if (e.Key == Key.NumPad0 || e.Key == Key.D0)
+                {
+                    time1stTxtBox.Text += "0";
+                    e.Handled = true;
+                }
+                else if (e.Key == Key.NumPad1 || e.Key == Key.D1)
+                {
+                    time1stTxtBox.Text += "1";
+                    e.Handled = true;
+                }
+                else if (e.Key == Key.NumPad2 || e.Key == Key.D2)
+                {
+                    time1stTxtBox.Text += "2";
+                    e.Handled = true;
+                }
+                else if (e.Key == Key.NumPad3 || e.Key == Key.D3)
+                {
+                    time1stTxtBox.Text += "3";
+                    e.Handled = true;
+                }
+                else if (e.Key == Key.NumPad4 || e.Key == Key.D4)
+                {
+                    time1stTxtBox.Text += "4";
+                    e.Handled = true;
+                }
+                else if (e.Key == Key.NumPad5 || e.Key == Key.D5)
+                {
+                    time1stTxtBox.Text += "5";
+                    e.Handled = true;
+                }
+                else if (e.Key == Key.NumPad6 || e.Key == Key.D6)
+                {
+                    time1stTxtBox.Text += "6";
+                    e.Handled = true;
+                }
+                else if (e.Key == Key.NumPad7 || e.Key == Key.D7)
+                {
+                    time1stTxtBox.Text += "7";
+                    e.Handled = true;
+                }
+                else if (e.Key == Key.NumPad8 || e.Key == Key.D8)
+                {
+                    time1stTxtBox.Text += "8";
+                    e.Handled = true;
+                }
+                else if (e.Key == Key.NumPad9 || e.Key == Key.D9)
+                {
+                    time1stTxtBox.Text += "9";
+                    e.Handled = true;
+                }
+            }
+            else if (time1stNums.Length >= 7 && time2ndNums.Length < 8)
+            {
+                if (time2ndNums.Length % 2 == 0 && time2ndNums.Length > 0)
+                {
+                    time2ndTxtBox.Text += ":";
+                }
+
+                if (e.Key == Key.NumPad0 || e.Key == Key.D0)
+                {
+                    time2ndTxtBox.Text += "0";
+                    e.Handled = true;
+                }
+                else if (e.Key == Key.NumPad1 || e.Key == Key.D1)
+                {
+                    time2ndTxtBox.Text += "1";
+                    e.Handled = true;
+                }
+                else if (e.Key == Key.NumPad2 || e.Key == Key.D2)
+                {
+                    time2ndTxtBox.Text += "2";
+                    e.Handled = true;
+                }
+                else if (e.Key == Key.NumPad3 || e.Key == Key.D3)
+                {
+                    time2ndTxtBox.Text += "3";
+                    e.Handled = true;
+                }
+                else if (e.Key == Key.NumPad4 || e.Key == Key.D4)
+                {
+                    time2ndTxtBox.Text += "4";
+                    e.Handled = true;
+                }
+                else if (e.Key == Key.NumPad5 || e.Key == Key.D5)
+                {
+                    time2ndTxtBox.Text += "5";
+                    e.Handled = true;
+                }
+                else if (e.Key == Key.NumPad6 || e.Key == Key.D6)
+                {
+                    time2ndTxtBox.Text += "6";
+                    e.Handled = true;
+                }
+                else if (e.Key == Key.NumPad7 || e.Key == Key.D7)
+                {
+                    time2ndTxtBox.Text += "7";
+                    e.Handled = true;
+                }
+                else if (e.Key == Key.NumPad8 || e.Key == Key.D8)
+                {
+                    time2ndTxtBox.Text += "8";
+                    e.Handled = true;
+                }
+                else if (e.Key == Key.NumPad9 || e.Key == Key.D9)
+                {
+                    time2ndTxtBox.Text += "9";
+                    e.Handled = true;
+                }
+            }
         }
-        public void TimeCalculator()
+        public void CalculateTime()
         {
-            if (dayTxtBox.Text == "")
+            string time1st = time1stTxtBox.Text;
+            string time2nd = time2ndTxtBox.Text;
+
+            string[] time1stParsed = time1st.Split(':');
+            string[] time2ndParsed = time2nd.Split(':');
+
+            long[] time1stIntArray = new long[time1stParsed.Length];
+
+            for (long i = 0; i < time1stParsed.Length; i++)
             {
-                dayTxtBox.Text = "00";
-            }
-            if (hourTxtBox.Text == "")
-            {
-                hourTxtBox.Text = "00";
-            }
-            if (minTxtBox.Text == "")
-            {
-                minTxtBox.Text = "00";
-            }
-            if (secTxtBox.Text == "")
-            {
-                secTxtBox.Text = "00";
+                if (long.TryParse(time1stParsed[i], out long num))
+                {
+                    time1stIntArray[i] = num;
+                }
             }
 
-            if (dayTxtBox2nd.Text == "")
+            long[] time2ndIntArray = new long[time2ndParsed.Length];
+
+            for (long i = 0; i < time2ndParsed.Length; i++)
             {
-                dayTxtBox2nd.Text = "";
-            }
-            if (hourTxtBox2nd.Text == "")
-            {
-                hourTxtBox2nd.Text = "";
-            }
-            if (minTxtBox2nd.Text == "")
-            {
-                minTxtBox2nd.Text = "";
-            }
-            if (secTxtBox2nd.Text == "")
-            {
-                secTxtBox2nd.Text = "";
+                if (long.TryParse(time2ndParsed[i], out long num))
+                {
+                    time2ndIntArray[i] = num;
+                }
             }
 
-            int days1st;
-            int hours1st;
-            int mins1st;
-            int secs1st;
-            int days2nd;
-            int hours2nd;
-            int mins2nd;
-            int secs2nd;
+            long days1st = 0;
+            long hours1st = 0;
+            long mins1st = 0;
+            long secs1st = 0;
+            long days2nd = 0;
+            long hours2nd = 0;
+            long mins2nd = 0;
+            long secs2nd = 0;
 
-            int days = 0;
-            int hours = 0;
-            int mins = 0;
-            int secs = 0;
-
-            if (int.TryParse(dayTxtBox.Text, out days1st) && int.TryParse(dayTxtBox2nd.Text, out days2nd))
+            if (time1stIntArray.Length > 0)
             {
-                days = days1st + days2nd;
+                days1st = time1stIntArray[0];
             }
-            if (int.TryParse(hourTxtBox.Text, out hours1st) && int.TryParse(hourTxtBox2nd.Text, out hours2nd))
+            if (time1stIntArray.Length > 1)
             {
-                hours = hours1st + hours2nd;
+                hours1st = time1stIntArray[1];
             }
-            if (int.TryParse(minTxtBox.Text, out mins1st) && int.TryParse(minTxtBox2nd.Text, out mins2nd))
+            if (time1stIntArray.Length > 2)
             {
-                mins = mins1st + mins2nd;
+                mins1st = time1stIntArray[2];
             }
-            if (int.TryParse(secTxtBox.Text, out secs1st) && int.TryParse(secTxtBox2nd.Text, out secs2nd))
+            if (time1stIntArray.Length > 3)
             {
-                secs = secs1st + secs2nd;
+                secs1st = time1stIntArray[3];
             }
 
-            if (days == 0 && hours == 0 && mins == 0 && secs == 0) { }
-            else
+            if (time2ndIntArray.Length > 0)
             {
-                mins = mins + secs / 60;
-                hours = hours + mins / 60;
-                days = days + hours / 24;
-
-                secs = secs % 60;
-                mins = mins % 60;
-                hours = hours % 24;
-
-                dayTxtBox.Text = days.ToString("D2");
-                hourTxtBox.Text = hours.ToString("D2");
-                minTxtBox.Text = mins.ToString("D2");
-                secTxtBox.Text = secs.ToString("D2");
-
-                dayTxtBox2nd.Text = "";
-                hourTxtBox2nd.Text = "";
-                minTxtBox2nd.Text = "";
-                secTxtBox2nd.Text = "";
+                days2nd = time2ndIntArray[0];
             }
+            if (time2ndIntArray.Length > 1)
+            {
+                hours2nd = time2ndIntArray[1];
+            }
+            if (time2ndIntArray.Length > 2)
+            {
+                mins2nd = time2ndIntArray[2];
+            }
+            if (time2ndIntArray.Length > 3)
+            {
+                secs2nd = time2ndIntArray[3];
+            }
+
+            long days = days1st + days2nd;
+            long hours = hours1st + hours2nd;
+            long mins = mins1st + mins2nd;
+            long secs = secs1st + secs2nd;
+
+            mins = mins + secs / 60;
+            hours = hours + mins / 60;
+            days = days + hours / 24;
+
+            secs = secs % 60;
+            mins = mins % 60;
+            hours = hours % 24;
+
+            string daysStr = days.ToString("D2");
+            string hoursStr = hours.ToString("D2");
+            string minsStr = mins.ToString("D2");
+            string secsStr = secs.ToString("D2");
+
+            string[] timeResArray = new string[4];
+            timeResArray[0] = daysStr;
+            timeResArray[1] = hoursStr;
+            timeResArray[2] = minsStr;
+            timeResArray[3] = secsStr;
+
+            time1stTxtBox.Text = string.Join(":", timeResArray);
+            time2ndTxtBox.Text = "";
         }
-
+        private void ParseNumber(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[0-9:]");
+            e.Handled = !regex.IsMatch(e.Text);
+        }
+        public void calculateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            CalculateTime();
+        }
         private void resetBtn_Click(object sender, RoutedEventArgs e)
         {
-            dayTxtBox.Text = "";
-            hourTxtBox.Text = "";
-            minTxtBox.Text = "";
-            secTxtBox.Text = "";
-            dayTxtBox2nd.Text = "";
-            hourTxtBox2nd.Text = "";
-            minTxtBox2nd.Text = "";
-            secTxtBox2nd.Text = "";
+            time1stTxtBox.Text = "";
+            time2ndTxtBox.Text = "";
         }
     }
 }
